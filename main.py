52e53463a3ee1b70ce4ad24822ae85e44feec233
Bohdan Rudenko <b.r.rudenko@email.com>
2025-10-09 10:51:20 +0300

    chore: change workername path, realese new version

--- main.py
+++ main.py
@@ -123,7 +123,7 @@ def get_or_create_worker_id(
     Якщо ні - генерує новий ID і зберігає у файл.
     """
     # Визначаємо шлях до файлу поруч з .exe
-    base_path = getattr(sys, "_MEIPASS", os.path.dirname(os.path.abspath(__file__)))
+    base_path = os.path.dirname(sys.executable)
     config_path = os.path.join(base_path, config_file)
     nickname_path = os.path.join(base_path, nickname_file)
 
